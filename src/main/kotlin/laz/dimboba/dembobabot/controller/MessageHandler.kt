4a49ae3d21f574fd169e0d06e0647ecb8147e0a2
Dimboba <lazarkovdmitry@yandex.ru>
2023-09-19 13:29:26 +0300

    fix: MessageHandler

--- src/main/kotlin/laz/dimboba/dembobabot/controller/MessageHandler.kt
+++ src/main/kotlin/laz/dimboba/dembobabot/controller/MessageHandler.kt
@@ -1,13 +1,24 @@
 package laz.dimboba.dembobabot.controller
 
 import dev.kord.core.event.message.MessageCreateEvent
+import laz.dimboba.dembobabot.exceptions.UnknownCommandException
 
 class MessageHandler(
     private val eventHandlers: List<MessageEventHandler>
 ) {
-    fun handleMessage(messageCreateEvent: MessageCreateEvent){
-        eventHandlers.forEach { _ ->
-            handleMessage(messageCreateEvent)
+    suspend fun handleMessage(messageCreateEvent: MessageCreateEvent){
+
+        var errors = 0;
+
+        for(handler in eventHandlers) {
+            try {
+                handler.handleMessage(messageCreateEvent)
+            } catch (ex: UnknownCommandException) {
+                errors++;
+            }
+        }
+        if(errors >= eventHandlers.size){
+            throw UnknownCommandException("Unknown command ${messageCreateEvent.message.content}")
         }
     }
 }
